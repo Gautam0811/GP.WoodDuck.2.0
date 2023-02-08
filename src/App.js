aaab45d8181f4649b4eb3d6252a3d026fc89911b
miraghav <miraghav@deloitte.com>
2023-02-08 18:21:14 +0530

    SalesGridHeader added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import WD_LeftSideBar from './components/WD_LeftSideBar';
 import WD_headerMiddleSection from './components/WD_header-middle-section';
 import React from 'react'; 
+import WD_SalesGridHeader from './components/WD_SalesGridHeader';
 
 function App() {
   return (
     <div>
       <WD_headerMiddleSection/>
+      <WD_SalesGridHeader></WD_SalesGridHeader>
       <WD_LeftSideBar />
     </div>
   );
