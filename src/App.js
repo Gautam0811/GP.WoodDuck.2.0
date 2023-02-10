7ca1939c7383eeefb4032fae764a69b3281de392
Gautam <gautmalhotra@deloitte.com>
2023-02-10 11:54:28 +0530

    React page routing added

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Grid from "@mui/material/Grid";
 import WD_headerMiddleSection from "./components/WD_header-middle-section";
 import WD_LeftSideBar from "./components/WD_LeftSideBar";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import WD_ProductsGroupHeader from "./components/WD_ProductGroupsHeader";
 import WD_ProductGroupsPage from "./components/WD_ProductGroupsPage";
 import WD_SalesGridHeader from "./components/WD_SalesGridHeader";
 function App() {
   return (
     <Grid>
-      <Grid>
-        <WD_headerMiddleSection />
-      </Grid>
-      <Grid sx={{ display: "flex", flexDirection: "row" }}>
-        <WD_LeftSideBar />
-        <WD_SalesGridHeader />
-      </Grid>
+      <Router>
+        <Grid>
+          <WD_headerMiddleSection />
+        </Grid>
+        <Grid sx={{ display: "flex", flexDirection: "row" }}>
+          <WD_LeftSideBar />
+
+          <Routes>
+            <Route path="/" element={<WD_SalesGridHeader />}></Route>
+            <Route path="/salesgrid" element={<WD_SalesGridHeader />}></Route>
+            <Route path="/productgroups" element={<WD_ProductGroupsPage />}></Route>            
+          </Routes>
+        </Grid>
+      </Router>
     </Grid>
   );
 }
--- src/components/WD_LeftSideBar.jsx
+++ src/components/WD_LeftSideBar.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import { Link } from "react-router-dom";
 import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
 import ArrowCircleLeftOutlinedIcon from "@mui/icons-material/ArrowCircleLeftOutlined";
 import SpeedIcon from "@mui/icons-material/Speed";
@@ -15,7 +15,6 @@ import BusinessOutlinedIcon from "@mui/icons-material/BusinessOutlined";
 import CachedOutlinedIcon from "@mui/icons-material/CachedOutlined";
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 
-
 function WD_LeftSideBar() {
   const { collapseSidebar } = useProSidebar();
 
@@ -23,23 +22,27 @@ function WD_LeftSideBar() {
     collapseSidebar();
   }, []);
 
- 
-
   return (
     <div id="app" style={{ display: "flex", height: "100vh" }}>
       <Sidebar backgroundColor="#434C56" style={{ height: "100vh" }}>
         <Menu sx={{ px: -2 }} style={{ color: "#d6dee9", fontSize: "10px" }}>
-          <MenuItem icon={<SpeedIcon style={{ fontSize: "16px" }} />}>
-            Dashboard
-          </MenuItem>
-          <MenuItem icon={<GridOnIcon style={{ fontSize: "16px" }} />}>
-            Sales Grid
-          </MenuItem>
-          <MenuItem
-            icon={<ShoppingCartOutlinedIcon style={{ fontSize: "16px" }} />}
-          >
-            Orders
-          </MenuItem>
+          <Link to="/" style={{ color: "#d6dee9" }}>
+            <MenuItem icon={<SpeedIcon style={{ fontSize: "16px" }} />}>
+              Dashboard
+            </MenuItem>
+          </Link>
+          <Link to="/salesgrid" style={{ color: "#d6dee9" }}>
+            <MenuItem icon={<GridOnIcon style={{ fontSize: "16px" }} />}>
+              Sales Grid
+            </MenuItem>
+          </Link>
+          <Link to="/productgroups" style={{ color: "#d6dee9" }}>
+            <MenuItem
+              icon={<ShoppingCartOutlinedIcon style={{ fontSize: "16px" }} />}
+            >
+              Orders
+            </MenuItem>
+          </Link>
           <MenuItem
             icon={<RateReviewOutlinedIcon style={{ fontSize: "16px" }} />}
           >
