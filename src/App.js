9bc1883c3918d37015192902a6e028aeab05048b
miraghav <miraghav@deloitte.com>
2023-02-13 14:19:36 +0530

    abcd  adde

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import WD_headerMiddleSection from './components/WD_header-middle-section';
 import WD_LeftSideBar from './components/WD_LeftSideBar';
 import React from 'react'; 
 import WD_SalesGridHeader from './components/WD_SalesGridHeader';
+import WD_SalesGrid from './components/WD_SalesGrid';
 
 function App() {
   return (
     <div>
       <div>
       <WD_headerMiddleSection/>
+      
+    </div>
+    <div style={{display: 'flex', flexDirection: 'row'}}>
       <WD_LeftSideBar />
-    </Fragment>
+      <WD_SalesGridHeader />
+      
+    </div>
+    <WD_SalesGrid/>
+    </div>
+    
   );
 }
 
