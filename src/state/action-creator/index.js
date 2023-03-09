dec64792a70ea39f3eb48ad38fd8a3ccdbf946d5
Krishna <62761249+krishnachoudhary274@users.noreply.github.com>
2023-03-09 16:59:51 +0530

    Update index.js

--- src/state/action-creator/index.js
+++ src/state/action-creator/index.js
@@ -1,3 +1,7 @@
+/*Component Name : Action-Creator
+ Utility : This action creator helps us to intialize the type and payload of action.
+ Author Krishna Choudharyn 06-03-2023-------------------------   */
+
 export const loginData = (amount) => {
 
   return (dispatch) => {
