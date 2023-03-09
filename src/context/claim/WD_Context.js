60e53c8dbc5a7d00f1b029dc700dce0bad1faa55
Krishna <62761249+krishnachoudhary274@users.noreply.github.com>
2023-03-09 16:58:04 +0530

    Update WD_Context.js

--- src/context/claim/WD_Context.js
+++ src/context/claim/WD_Context.js
@@ -1,3 +1,7 @@
+/*Component Name : WDContext
+ Utility : This Component is responsible to initialize the context. 
+ Author Krishna Choudharyn 06-03-2023-------------------------   */
+
 import { createContext } from "react";
 
 const wdContext = createContext();
