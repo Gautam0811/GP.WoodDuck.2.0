4c79806b7901e74b96df25af57f9f4c83a0da302
Gautam <gautmalhotra@deloitte.com>
2023-03-15 19:25:42 +0530

    Changes

--- src/components/businessComponents/dashboard/Dashboard.tsx
+++ src/components/businessComponents/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@
 // Component Utility : The Component is the main Dashboard component which has all the reusable components and is rendered on the app 
 // Author Gautam Malhotra on 1-3-2023
 // -------------------------
+import * as React from "react";
 import Grid from "@mui/material/Grid";
 import DashboardMarket from "./DashboardMarket";
 import DashboardObjects from "../../common/DashboardObjects";
