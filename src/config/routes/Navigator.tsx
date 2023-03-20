85aaf760aa86d67bcc485ec1af9296113e81ca71
ananydhar <ananyadhar@somaiya.edu>
2023-03-20 21:42:52 +0530

    added footer and leftsidebar

--- src/config/routes/Navigator.tsx
+++ src/config/routes/Navigator.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import {Routes, Route} from "react-router-dom";
-import Dashboard from "../../features/businessComponents/dashboard/Dashboard"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../../features/businessComponents/dashboard/Dashboard";
+import LeftSideBar from "../../pages/common/LeftSideBar";
+import Footer from "../../pages/common/Footer";
 
 export function Navigator() {
-    return(
-    <Routes>
-        <Route path="/" element={<Dashboard/>}> </Route>
-        {/* <Route path="/salesgrid" element={<SalesGrid />}></Route>
+  return (
+    <div>
+      <div className="flexrow">
+        <LeftSideBar />
+        <Routes>
+          <Route path="/" element={<Dashboard />}>
+            {" "}
+          </Route>
+          {/* <Route path="/salesgrid" element={<SalesGrid />}></Route>
         <Route path="/productgroups" element={<ProductGroupsPage />}></Route>
         <Route path="/settings" element={<Settings/>} > </Route>   
         <Route path="/orders" element={<Orders/>} > </Route> 
         <Route path="/contracts" element={<NewContract/>} > </Route>
         <Route path="/Adminpermissions" element={<Permissions data={PermissionsData}/>} > </Route> */}
-    </Routes>
-    )
-
+        </Routes>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/common/Footer.tsx
@@ -0,0 +1,13 @@
+// WD_Footer
+// Component Utility : The Component is created to render a footer on each page in the app
+// Author Ananya Dhar on 1-3-2023
+// -------------------------
+
+export default function Footer() {
+  return (
+    <div className="fs-12 p-32 flex-center gray" >
+        <span>© 2023 Georgia-Pacific. All rights reserved.</span> < br />
+        <span>Component Time: / API: </span>
+    </div>
+  );
+}
