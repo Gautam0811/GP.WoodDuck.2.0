f7c6bc3523c57b2b551cf029f1130c0e58671042
ananydhar <ananyadhar@somaiya.edu>
2023-03-23 23:19:07 +0530

    added click my profile feature

--- src/globalPages/common/header/headerSection/RightHeader.tsx
+++ src/globalPages/common/header/headerSection/RightHeader.tsx
@@ -2,6 +2,8 @@
 // Component Utility : The Component created for rendering a header navigation bar for all the pages in the app
 // Author Gautam Malhotra on 1-3-2023
 // -------------------------
+
+
 import * as React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
@@ -20,46 +22,91 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import "../../../../styles/StyleMain.css";
+import {Grid, Stack, Avatar, Modal, Button} from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 
 
 export  function RightHeader() {
   const [post, setPost]:any = React.useState({});
   const [name, setName]:any = React.useState({});
 
-  React.useEffect((() => setName(JSON.parse(localStorage.getItem('status') || '{}'))), []);
-  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  // React.useEffect((() => setName(JSON.parse(localStorage.getItem('status') || '{}'))), []);
+
+  //React.useEffect(() => setName(JSON.parse(localStorage.getItem('status') || '{}')), []);
+
+  React.useEffect(() => {
+     function fetchName() {
+      let nameFetch= JSON.parse(localStorage.getItem('status') || '{}')
+      setName(nameFetch)
+    }
+    fetchName()
+  }, [])
+
+  var nameByChar= JSON.parse(localStorage.getItem('status') || '{}').name.charAt(0)
+
+
   const logout = () => {
     localStorage.clear();
     window.location.reload();
   };
+
   React.useEffect(() => {
     window.addEventListener("storage", logout);
   }, [logout]);
   return (
     // View for header middle section
-    <Box className="fontsize" sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ background: "#ffb74d" }}>
+    <Box className="fs-12 flex-grow">
+      <AppBar className="bg-ffb74d position-static"  >
         <Toolbar>
-          <FormControl
-            sx={{
-              m: 1,
-              minWidth: 120,
-              position: "relative",
-              left: 300,
-            }}
-            style={{ background: "white" }}
-          >
-          </FormControl>
-         
+          <FormControl></FormControl>
+
           <Typography
             variant="h6"
             component="div"
             color={"primary"}
-            align="center"
-            sx={{ flexGrow: 1, position: "absolute", right: 10 }}
+            className="header-right"
+            
           >
-            {name.name}
-            <Typography variant="caption" component="div" color={"black"}>
+            <Grid className="flexrow">
+              <Typography>{name.name}</Typography>
+              <Grid>
+                <Button onClick={handleOpen}>
+                  <Stack spacing={2}>
+                    <Avatar className="w25-h25 fs-16">{nameByChar}</Avatar>
+                  </Stack>
+                </Button>
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                >
+                  <Box className="modal-header-right">
+                  <div className="flex-end">
+                  <Button><LogoutIcon /></Button>
+                  </div>
+                    <Typography
+                      variant="h6"
+                      component="h2"
+                      color="primary"
+                    >
+                      {name.name}
+                    </Typography>
+                    <Typography color="primary">
+                      Email
+                    </Typography>
+                    <Typography color="primary">
+                      Permission Set
+                    </Typography>
+                  </Box>
+                </Modal>
+              </Grid>
+            </Grid>
+            <Typography className="font-black" variant="caption" component="div" >
               <IconButton size="small" color="primary" aria-label="menu">
                 <MenuIcon fontSize="small" />
               </IconButton>
@@ -83,7 +130,12 @@ export  function RightHeader() {
                   <EmailOutlinedIcon fontSize="small" />
                 </Badge>
               </IconButton>
-              <IconButton size="small" color="primary" aria-label="logoutbutton" onClick={logout}>
+              <IconButton
+                size="small"
+                color="primary"
+                aria-label="logoutbutton"
+                onClick={logout}
+              >
                 <LogoutOutlinedIcon fontSize="small" />
               </IconButton>
             </Typography>
