45cdf3831b1da1ddbd4241611331966ec6740083
Gautam <gautmalhotra@deloitte.com>
2023-03-23 14:52:11 +0530

    header added

--- src/globalPages/home/Home.tsx
+++ src/globalPages/home/Home.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Navigator } from "../../routes/index";
-import LeftSideBar from "../../globalPages/common/LeftSideBar";
-import Footer from "../../globalPages/common/Footer";
-import Login from "../login/Login";
-import Header from "../../features/header/Header";
+import {LeftSideBar} from "../../globalPages/common/leftSideBar/index";
+import {Footer} from "../../globalPages/common/footer/index";
+import {Login} from "../login/index";
+import {Header} from "../../globalPages/common/header/index";
 
-export default function Home() {
+export function Home() {
   const token: any = localStorage.getItem("token");
   if (!token) {
     return <Login />;
