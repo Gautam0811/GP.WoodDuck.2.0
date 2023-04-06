23e4d0f0404095cba9652295536177c97f45f1b9
krishna choudhary <kchoudhary274@gmail.com>
2023-04-06 16:06:07 +0530

    updated

--- src/features/settings/index.tsx
+++ src/features/settings/index.tsx
@@ -2,4 +2,3 @@ export {Settings} from "./component/Settings";
 export {Permissions} from "./component/adminSettings/adminPermissions/Permissions";
 export {ManageUsers} from "./component/siteSettings/manageUsers/component/ManageUsers";
 export {PermissionsData } from "./services/Data";
-export {permissionSetData } from "./services/Data";
