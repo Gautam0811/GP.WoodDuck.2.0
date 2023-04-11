570869a30b8002bfd8661baa3601bea738fd0d55
ananydhar <ananyadhar@somaiya.edu>
2023-04-11 12:02:04 +0530

    updated subdivision filter

--- src/globalPages/common/header/headerSection/subdivision/Subdivision.tsx
+++ src/globalPages/common/header/headerSection/subdivision/Subdivision.tsx
@@ -7,40 +7,40 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import "../../../../../styles/StyleMain.css";
 import { Grid, Modal, Button, Box, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 export function Subdivision() {
   const [name, setName]: any = React.useState({});
+  const [subdivision, setSubdivision] = React.useState("");
   React.useEffect(() => {
     function fetchName() {
       let nameFetch = JSON.parse(localStorage.getItem("status") || "{}");
       setName(nameFetch);
+      setSubdivision(nameFetch.subdivision)
+      //on first time load
+      window.localStorage.setItem("subdivisionValue",nameFetch.subdivision);
     }
     fetchName();
   }, []);
 
-
-  const [subdivision, setSubdivision] = React.useState(name.subdivision);
-
   const handleChange = (event: SelectChangeEvent) => {
     setSubdivision(event.target.value);
+    //on select of subdivision load
+    window.localStorage.setItem("subdivisionValue",event.target.value);
   };
-
-
   return (
     // View for Subdivision section
     <Grid className="flex-end col-005fa8">
       <Grid>
         <FormControl variant="standard" className="col-005fa8">
+        {/* <InputLabel id="demo-simple-select-standard-label">{name.subdivision}</InputLabel> */}
         <Select
           id="demo-simple-select-standard"
           value={subdivision}
           onChange={handleChange}
           className="col-005fa8"
         >
-          <MenuItem value={10} className="col-005fa8">SL</MenuItem>
-          <MenuItem value={20} className="col-005fa8">SP</MenuItem>
+          <MenuItem value={"SL"} className="col-005fa8">SL</MenuItem>
+          <MenuItem value={"SP"} className="col-005fa8">SP</MenuItem>
         </Select>
         
       </FormControl>
--- src/modules/settings/component/siteSettings/manageUsers/component/ManageUsersTabs.tsx
+++ src/modules/settings/component/siteSettings/manageUsers/component/ManageUsersTabs.tsx
@@ -10,95 +10,64 @@ import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import { IconButton, Tabs, Tab } from "@mui/material";
-import {ManageUsersGrid } from './index';
-import AppBar from '@mui/material/AppBar';
-
+import { ManageUsersGrid } from "./index";
+import AppBar from "@mui/material/AppBar";
 
 export function ManageUsersTabs() {
-
   const [value, setValue] = React.useState(0);
-  const [isActive, setIsActive]:any=React.useState(true);
-
+  const [isActive, setIsActive]: any = React.useState(true);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
-  };
-function setValueActive()
-{
-    setIsActive(true); 
-}
-function setValueInactive()
-{
-    
-    setIsActive(false);
    
-}
+  };
+  function setValueActive() {
+    setIsActive(true);
+  }
+  function setValueInactive() {
+    setIsActive(false);
+  }
 
   interface TabPanelProps {
     children: React.ReactNode;
     value: number;
     index: number;
-}
- function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+  }
+  function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  //this returns the index when tab changes
+
   function a11yProps(index: number) {
     //alert(index);
     return {
-        id: `full-width-tab-${index}`,
-        'aria-controls': `full-width-tabpanel-${index}`,
-      };
+      id: `full-width-tab-${index}`,
+      "aria-controls": `full-width-tabpanel-${index}`,
+    };
   }
   return (
-    //This is the header for Manage Users
+    //This is the tabs header for Manage Users
 
     <Box>
       <Grid className="bg-grey-white pl-16 w100 align-items-center">
-        <Box>
-          {/* <AppBar position="static">
-          <Tabs value={value} onChange={handleChange}>
-            <Tab value="0" label="Active" {...a11yProps(0)} onChange={setValueActive}></Tab>
-            
-            <Tab value="1" label="Inactive" {...a11yProps(1)} onChange={setValueInactive}/>
-            <Tab value="2" icon={<RefreshIcon />} {...a11yProps(2)}/>
-          </Tabs>
-          </AppBar> */}
-          {/* {value === 0 && <TabPanel>Content for Active</TabPanel>}
-          {value === 1 && <TabPanel>Content for Inactive</TabPanel>}
-          {value === 2 && <TabPanel>Content for Refresh</TabPanel>} */}
-          {/* <TabPanel value={value} index={value}>
-            Content for TAB 1
-          </TabPanel>
-          <TabPanel value={value} index={value}>
-            Content for TAB 2
-          </TabPanel>
-          <TabPanel value={value} index={value}>
-            Content for TAB 3
-          </TabPanel> */}
-          {/* { value === 0 ? <TabPanel value={0} index={0}>1111</TabPanel> : <TabPanel value={1} index={1}>6222</TabPanel>} */}
-          {/* {value === 0 && <TabPanel>Content for Active</TabPanel>}
-          {value === 1 && <TabPanel>Content for Inactive</TabPanel>}
-          {value === 2 && <TabPanel>Content for Refresh</TabPanel>} */}
-          {/* {value === 0 && <TabPanel value={value} index={0}><ManageUsersGrid /></TabPanel>} */}
-          {/* {value === 1 && <TabPanel value={value} index={1}><ManageUsersGrid /></TabPanel>}
-          {value === 2 && <TabPanel value={value} index={2}><ManageUsersGrid /></TabPanel>} */}
-        </Box>
+        <Box></Box>
       </Grid>
       <Grid className="bg-grey-white pl-16 w100 align-items-center">
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
@@ -118,10 +87,10 @@ function setValueInactive()
         </Box>
       </Grid>
       <TabPanel value={value} index={0}>
-        <ManageUsersGrid isAcitve={isActive} />
+        <ManageUsersGrid isActive={isActive} selectedDivision={ window.localStorage.getItem("subdivisionValue")} />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <ManageUsersGrid isAcitve={isActive}/>
+        <ManageUsersGrid isActive={isActive} selectedDivision={ window.localStorage.getItem("subdivisionValue")}/>
       </TabPanel>
       <TabPanel value={value} index={2}>
         <RefreshIcon />
@@ -129,93 +98,3 @@ function setValueInactive()
     </Box>
   );
 }
-
-//another code example
-
-// import React, { useState } from "react";
-// import { DataGrid, GridColDef } from "@mui/x-data-grid";
-
-// const rows = [
-//     {id: 1, name: 'AD', age: 25, city: "New York" },
-//     {id: 2, name: 'AD', age: 25, city: "Ne York" },
-//     {id: 3, name: 'AD', age: 25, city: "Nework" }
-// ]
-
-// const columns: GridColDef[] = [
-//     { field: "id", headerName: 'ID', width: 70},
-//     { field: "name", headerName: 'name', width: 70},
-//     { field: "age", headerName: 'age', width: 70},
-//     { field: "city", headerName: 'city', width: 70},
-// ]
-
-// export const ManageUsersTabs = () => {
-//     const [filterModel, setFilterModel] =   useState({items: [] });
-
-//     const handleFilterModelChange = (model: any) => {
-//         setFilterModel(model);
-//     };
-
-//     return (
-//         <div style={{ height: 400, width: "100%"}}>
-//             <DataGrid 
-//                 checkboxSelection
-//                 rows={rows}
-//                 columns={columns}
-//                 // filterModel = {filterModel}
-//                 onFilterModelChange={handleFilterModelChange}
-//                 />
-//         </div>
-//     )
-// }
-
-//another example
-
-// import React, {useState} from 'react';
-// import  {DataGrid , GridToolbar } from '@mui/x-data-grid';
-// import Button from '@mui/material/Button';
-
-// const rows = [
-//     {id: 1, name: 'AD', age: 25, city: "New York" },
-//     {id: 2, name: 'AD', age: 25, city: "Ne York" },
-//     {id: 3, name: 'AD', age: 25, city: "Nework" }
-// ]
-
-// const columns: GridColDef[] = [
-//     { field: "id", headerName: 'ID', width: 70},
-//     { field: "name", headerName: 'name', width: 70},
-//     { field: "age", headerName: 'age', width: 70},
-//     { field: "city", headerName: 'city', width: 70},
-// ]
-
-// export function ManageUsersTabs() {
-//     const [filteredRows, setFilteredRows] = useState([]);
-
-//     const handleFilter = ( filterValue) => {
-//         const filteredData = rows.filter((row) => {
-//             return row.city.toLowerCase().includes(filterValue.toLowerCase());
-//         });
-//         setFilteredRows(filteredData);
-//     };
-
-//     return (
-//         <div>
-//             <DataGrid rows ={filteredRows.length > 0 ? filteredRows : rows}
-//             columns={columns}
-//             components = {{
-//                 Toolbar: GridToolbar,
-//             }}
-//             componentsProps={{
-//                 toolbar: {
-//                     filterComponent: (
-//                         <FilterComponent onFilter = {handleFilter} />
-//                     ),
-//                 },
-//             }}
-//             />
-//         </div>
-//     );
-// };
-
-
-
-
